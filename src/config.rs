a5aa98b1fc258c6cb5667809a3f3fc33b86485fb
Hewston Fox <hewstonfox@gmail.com>
2024-01-04 00:29:48 +0200

    feat: add implement watchers update in config change

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,25 @@
-use std::sync::{Arc, Mutex};
+use parking_lot::{Mutex};
 use std::collections::HashMap;
 use std::fs;
 use std::fs::OpenOptions;
 use std::io::{Read, Seek};
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use std::sync::mpsc::Sender;
+use notify_debouncer_full::DebounceEventResult;
 use serde::{Deserialize, Serialize};
+use serde_diff::{SerdeDiff};
 
 const CONFIG_FILE: &str = "config.json";
 
-#[derive(Serialize, Deserialize, Copy, Clone, Debug)]
+#[derive(SerdeDiff, Serialize, Deserialize, Copy, Clone, Debug)]
 #[serde(rename_all = "camelCase")]
 pub enum AccessRights {
     Read,
     Write,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(SerdeDiff, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct SherryConfigSourceJSON {
     pub id: String,
@@ -28,20 +32,32 @@ pub struct SherryConfigSourceJSON {
 }
 
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(SerdeDiff, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct SherryConfigWatcherJSON {
     pub source: String,
     pub local_path: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(SerdeDiff, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct SherryConfigJSON {
     pub sources: HashMap<String, SherryConfigSourceJSON>,
     pub watchers: Vec<SherryConfigWatcherJSON>,
     pub webhooks: Vec<String>,
 }
+
+impl SherryConfigJSON {
+    pub fn clone(&self) -> SherryConfigJSON {
+        serde_json::from_str(serde_json::to_string(self).unwrap().as_str()).unwrap()
+    }
+}
+
+pub struct SherryConfigUpdateEvent {
+    pub old: SherryConfigJSON,
+    pub new: SherryConfigJSON,
+}
+
 
 pub fn get_default_config() -> SherryConfigJSON {
     SherryConfigJSON {
@@ -113,20 +129,29 @@ pub fn initialize_config_dir(dir: &PathBuf) -> Result<(), ()> {
     Ok(())
 }
 
-pub fn get_config_watch_cb(dir: PathBuf, config_mutex: Arc<Mutex<SherryConfigJSON>>) -> impl Fn(notify::Result<notify::Event>) {
+pub fn get_config_watch_cb(dir: PathBuf, config_mutex: Arc<Mutex<SherryConfigJSON>>, sender: Sender<SherryConfigUpdateEvent>) -> impl Fn(DebounceEventResult) {
     let config_dir = dir.join(CONFIG_FILE);
     let dir = dir.clone();
-    move |res: notify::Result<notify::Event>| {
+    let owned_sender = sender.clone();
+    move |res: DebounceEventResult| {
         if res.is_err() {
             return;
         }
 
         let event = res.unwrap();
-        for path in &event.paths {
-            if config_dir.eq(path) {
-                let mut data = config_mutex.lock().unwrap();
-                if let Ok(config) = read_config(&dir) {
-                    *data = config
+        for event in &event {
+            for path in &event.paths {
+                if config_dir.eq(path) {
+                    let mut data = config_mutex.lock();
+                    if let Ok(config) = read_config(&dir) {
+                        owned_sender.send(SherryConfigUpdateEvent {
+                            old: (*data).clone(),
+                            new: config.clone()
+                        }).unwrap();
+
+                        *data = config;
+                        println!("Config updated {:?}", *data);
+                    }
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ mod config;
 
 use clap::Parser;
 use std::{env};
-use std::sync::{Arc, Mutex};
+use parking_lot::{Mutex};
+use std::sync::{Arc};
 use std::path::{Path, PathBuf};
+use std::sync::mpsc::{channel, Receiver};
 use std::time::Duration;
 use home::home_dir;
-use notify::{RecursiveMode, Watcher};
-use notify_debouncer_full::new_debouncer;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use notify_debouncer_full::{Debouncer, FileIdMap, new_debouncer};
 use path_clean::PathClean;
+use crate::config::SherryConfigUpdateEvent;
 
 const CONFIG_DIR: &str = ".sherry";
 
@@ -18,10 +21,8 @@ struct Args {
     config: Option<String>,
 }
 
-fn main() {
-    let args = Args::parse();
-
-    let config_dir: PathBuf = match args.config {
+fn resolve_config_dir(config: Option<String>) -> PathBuf {
+    match config {
         Some(config) => {
             let path = PathBuf::from(config);
             if path.is_absolute() {
@@ -31,75 +32,76 @@ fn main() {
             }.clean()
         }
         None => home_dir().unwrap().join(CONFIG_DIR)
-    };
+    }
+}
 
-
+fn initialize_config_watcher(config_dir: &PathBuf) -> Option<(Arc<Mutex<config::SherryConfigJSON>>, Debouncer<RecommendedWatcher, FileIdMap>, Receiver<SherryConfigUpdateEvent>)> {
     println!("Using configuration from: {:?}", config_dir);
 
-    if config::initialize_config_dir(&config_dir).is_err() {
+    if config::initialize_config_dir(config_dir).is_err() {
         println!("Unable to initialize configuration, maybe access is denied");
-        return;
+        return None;
     }
 
     let shared_config: Arc<Mutex<config::SherryConfigJSON>> = Arc::new(Mutex::new(config::read_config(&config_dir).unwrap()));
 
-    let mut config_watcher = notify::recommended_watcher(
-        config::get_config_watch_cb(config_dir.clone(), Arc::clone(&shared_config))
+    let (tx, rx) = channel::<SherryConfigUpdateEvent>();
+
+    let mut config_debounce = new_debouncer(
+        Duration::from_millis(200),
+        None,
+        config::get_config_watch_cb(config_dir.clone(), Arc::clone(&shared_config), tx)
     ).unwrap();
-    config_watcher.watch(config_dir.as_path(), RecursiveMode::Recursive).unwrap();
 
+    config_debounce.watcher().watch(config_dir.as_path(), RecursiveMode::Recursive).unwrap();
 
+    return Some((shared_config, config_debounce, rx));
+}
 
-    let watchers_config = Arc::clone(&shared_config);
+fn initialize_watchers(shared_config: Arc<Mutex<config::SherryConfigJSON>>) -> Debouncer<RecommendedWatcher, FileIdMap> {
+    let main_watcher_config = Arc::clone(&shared_config);
     let mut debouncer = new_debouncer(Duration::from_millis(200), None, move |results| {
         if let Ok(results) = results {
-            let config = watchers_config.lock().unwrap();
+            let config = main_watcher_config.lock();
             println!("Config: {:?}", *config);
             for result in results {
                 println!("Result: {:?}", result);
             }
         }
     }).unwrap();
+
     let watcher = debouncer.watcher();
     {
-        let config = shared_config.lock().unwrap();
+        let config = shared_config.lock();
         for watcher_path in config.watchers.iter() {
             watcher.watch(Path::new(&watcher_path.local_path), RecursiveMode::Recursive).unwrap();
         }
     }
 
-    loop {}
+    return debouncer;
+}
 
-    // let paths = initialize();
-    // println!("{:?}", paths);
-    //
-    // let async_block = async {
-    //     let (mut tx, mut rx) = unbounded();
-    //
-    //     let mut watcher = recommended_watcher(move |res| {
-    //         futures::executor::block_on(async {
-    //             tx.send(res).await.unwrap();
-    //         })
-    //     })
-    //         .unwrap();
-    //
-    //     watcher
-    //         .watch(Path::new("../test-dirs/a"), RecursiveMode::Recursive)
-    //         .unwrap();
-    //     watcher
-    //         .watch(Path::new("../test-dirs/b"), RecursiveMode::Recursive)
-    //         .unwrap();
-    //     watcher
-    //         .watch(Path::new("../test-dirs/c"), RecursiveMode::Recursive)
-    //         .unwrap();
-    //
-    //     while let Some(res) = rx.next().await {
-    //         match res {
-    //             Ok(event) => println!("changed: {:?}", event),
-    //             Err(e) => println!("watch error: {:?}", e),
-    //         }
-    //     }
-    // };
-    //
-    // futures::executor::block_on(async_block);
+fn listen_config_update(watcher: &mut RecommendedWatcher, rx: Receiver<SherryConfigUpdateEvent>) {
+    for update in rx {
+        // TODO: Check the diff correctly
+
+        for watcher_path in update.old.watchers.iter() {
+            watcher.unwatch(Path::new(&watcher_path.local_path)).unwrap();
+        }
+        for watcher_path in update.new.watchers.iter() {
+            watcher.watch(Path::new(&watcher_path.local_path), RecursiveMode::Recursive).unwrap();
+        }
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let config_dir = resolve_config_dir(args.config);
+
+    let config_args = initialize_config_watcher(&config_dir);
+    if config_args.is_none() { return; }
+    let (shared_config, _config_debounce, rx) = config_args.unwrap();
+
+    listen_config_update(initialize_watchers(shared_config).watcher(), rx);
 }
