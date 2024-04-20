df440fa2cbba038462b24fb17bc067fc4c33eec1
Hewston Fox <hewstonfox@gmail.com>
2024-04-21 01:35:14 +0300

    fix: initial hash creation

--- src/hash.rs
+++ src/hash.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
-use std::path::{Path, PathBuf};
+use std::fs;
+use std::path::PathBuf;
 
 use glob::glob;
 use serde::{Deserialize, Serialize};
@@ -7,7 +8,7 @@ use serde_diff::SerdeDiff;
 
 use crate::config::SherryConfigSourceJSON;
 use crate::constants::HASHES_DIR;
-use crate::helpers::initialize_json_file_with;
+use crate::helpers::{initialize_json_file_with, str_err_prefix};
 
 #[derive(SerdeDiff, Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 #[serde(rename_all = "camelCase")]
@@ -22,7 +23,7 @@ pub fn get_file_hash(path: &PathBuf) -> String {
     if path.is_dir() {
         return "".to_string();
     }
-    match std::fs::read(path) {
+    match fs::read(path) {
         Ok(content) => {
             seahash::hash(&content).to_string()
         }
@@ -33,17 +34,25 @@ pub fn get_file_hash(path: &PathBuf) -> String {
 }
 
 fn build_hashes(hashes_id: &String, source: &SherryConfigSourceJSON, local_path: &PathBuf) -> WatcherHashJSON {
+    let binding = local_path.join("**/*");
+    let to_search = binding.to_str().unwrap();
+    println!("TO SEARCH {}", &to_search);
+    let glob_files = glob(to_search).unwrap();
+
     WatcherHashJSON {
         id: hashes_id.clone(),
         source_id: source.id.clone(),
         local_path: local_path.to_str().unwrap().to_string(),
-        hashes: glob(Path::new(local_path).join("**").to_str().unwrap()).unwrap().filter_map(|v| {
+        hashes: glob_files.filter_map(|v| {
             let res = v.unwrap();
+            println!("RES: {}", &res.to_str().unwrap());
             return if res.is_file() { Some((res.to_str().unwrap().to_string(), get_file_hash(&res))) } else { None };
         }).into_iter().collect(),
     }
 }
 
 pub fn get_hashes(dir: &PathBuf, source: &SherryConfigSourceJSON, local_path: &PathBuf, hashes_id: &String) -> Result<WatcherHashJSON, String> {
-    initialize_json_file_with(&dir.join(HASHES_DIR).join(hashes_id), &|| { build_hashes(hashes_id, source, local_path) })
+    let hashes_dir = dir.join(HASHES_DIR);
+    fs::create_dir_all(&hashes_dir).map_err(str_err_prefix("Error hashes dir creation"))?;
+    initialize_json_file_with(&hashes_dir.join(format!("{}.json", hashes_id)), &|| { build_hashes(hashes_id, source, local_path) })
 }
